ae8a8d445d71adca2cd4194432b494ec2b10cc78
VinVaz <viniciusvazpro@gmail.com>
2018-05-29 18:25:49 -0300

    initial structure

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+module.exports = {
+	entry: './source/app.js',
+	output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: 'gameoflife.js'
+    },
+	module: {
+      rules: [
+        {
+          test: /\.js$/,
+          use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['react', 'env'],
+              plugins: ['transform-class-properties']
+            }
+           }
+          ],
+           exclude: path.resolve(__dirname, 'node_modules')
+        },
+		{
+          test: /\.css$/,
+		  use: [
+            { loader: "style-loader" },
+            { loader: "css-loader" }
+          ] 
+		},
+		{
+		  test: /\.(png|jpg|gif)$/,
+		  use: [
+			{
+			  loader: 'url-loader',
+			  options: {
+				mimetype: 'image/png'
+			  }
+			}
+		  ]
+        }
+      ]
+    }
+};
