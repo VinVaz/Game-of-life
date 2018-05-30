d94dfc84b09f7297d49b67ce959ebb876b43b7b4
VinVaz <viniciusvazpro@gmail.com>
2018-05-30 12:52:03 -0300

    update

--- source/components/Generation.js
+++ source/components/Generation.js
@@ -7,12 +7,20 @@ const gridCells = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 
 class Generation extends Component{
     state = {
 		grid: gridCells
-	}	
+	}
+	getNextGrid = (nextGrid) => {
+		this.setState({
+			grid: nextGrid
+		});
+	}
 	render(){
 	  const {grid} = this.state;
 	  return(
         <div>
-		  <NextGeneration/>
+		  <NextGeneration 
+		    grid={grid}
+			getNextGrid={this.getNextGrid}
+		  />
           <Grid grid={grid}/>		
 	    </div>
 	  );	
--- source/components/NextGeneration.js
+++ source/components/NextGeneration.js
@@ -1,9 +1,35 @@
 import React, {Component} from "react";
+import Handler from "./Handler.js";
 
-class NextGeneration extends Component{	
+const grid = [0, 1, 0];
+
+class NextGeneration extends Component{
+	state={
+		nextGeneration: []
+	}
+	activateNextGeneration = () => {
+		const {getNextGrid} = this.props;
+		const {nextGeneration} = this.state;
+		getNextGrid(nextGeneration);
+	}
+	updateGrid = (val, index) => {
+		const {nextGeneration} = this.state;
+		nextGeneration[index] = val;
+		setState({
+			nextGeneration: nextGeneration
+		});
+	}
+	calculateEach = () => {
+		return grid.map((val, index) => {
+			return <Handler state={val} index={index} updateGrid={this.updateGrid}/>
+		})
+	}
+	
 	render(){
+	  //const {grid} = this.props;
 	  return(
-        <div>		
+        <div>
+		  <button onClick={this.activateNextGeneration} ></button>		
 	    </div>
 	  );	
 	}
